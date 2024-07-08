86878ad2b8b4852ac9c0df63924809cdd5ed499f
sufal54 <145686201+sufal54@users.noreply.github.com>
2024-07-08 01:00:57 -0400

    Add files via upload

---
+++ client/src/utils/supportfunction.js
@@ -0,0 +1,13 @@
+export const filter = [
+    {id:1,name:"Romantic",value:"romantic"},
+    {id:2,name:"Rock",value:"rock"},
+    {id:3,name:"Classical",value:"classical"},
+    {id:4,name:"Others",value:"other"}
+];
+
+export const filterByLanguage = [
+    {id:1,name:"English",value:"english"},
+    {id:2,name:"Hindi",value:"hindi"},
+    {id:3,name:"Bengoli",value:"bengoli"},
+    {id:4,name:"Others",value:"other"}
+];
